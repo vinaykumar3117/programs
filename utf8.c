c4c8a4ac646d0a7196c0cb68223457d07d3d566d
vinaykumar3117 <35131302+vinaykumar3117@users.noreply.github.com>
2018-05-26 18:28:57 +0530

    Print UTF8 chars

---
+++ utf8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	if (!setlocale(LC_CTYPE, "")) 
+	{
+      	printf("Can't set the specified locale! Check LANG, LC_CTYPE, LC_ALL.\n");
+    	return 1;
+    }
+    printf("%ls\n", L"Schöne Grüße");
+    return 0;
+}
